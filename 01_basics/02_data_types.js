8a1a8767d2b9d9928c8de9afd2e1815400b258ed
praveen9569 <144908644+praveen9569@users.noreply.github.com>
2025-01-21 19:52:14 +0000

    data_types

---
+++ 01_basics/02_data_types.js
@@ -0,0 +1,13 @@
+//alert(3+3)  we using node.js not broswer ,,we differnt method to using alert
+
+//console.log(typeof null);
+
+//string 
+//integer
+//boolean
+//null
+//undefined
+//symbol
+//object
+
+console.log("ram")
