2905429efccb672106e4ecd4ef80a015182f3b6c
praveen9569 <144908644+praveen9569@users.noreply.github.com>
2025-01-21 19:33:54 +0000

    about variables in js

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,7 @@
+const accountid = 2323;
+var accountName = "raju";
+let jk = "ram"
+// const variable can't be chaged using the in the code further;;
+  
+console.table([accountid,accountName,jk]);
+console.log(accountName);
