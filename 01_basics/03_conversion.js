39e0411ded95ff3ba7b3c01d24909278a2e119a4
praveen9569 <144908644+praveen9569@users.noreply.github.com>
2025-01-21 20:06:21 +0000

    conversion from one data type to another data type in js

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,16 @@
+let score ="33"
+console.log(typeof( score))
+
+
+let value = Number(score)
+console.log(typeof(value))
+
+//basics conversion
+
+//"33"-->33
+//"333abs" -->NaN(not a number)
+//  true -->1;   false-->0
+
+let  num  = 5;
+let name = String(num)
+console.log(typeof(name))
