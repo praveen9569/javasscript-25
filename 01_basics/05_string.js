5c3885f65ff29239aa94678a06cbae0ce9c319d1
praveen9569 <144908644+praveen9569@users.noreply.github.com>
2025-01-28 08:52:05 +0000

    string property

---
+++ 01_basics/05_string.js
@@ -0,0 +1,15 @@
+const name=  "praveen";
+const name1 = "kumar";
+// console.log(name+name1+"nishad")
+// console.log(`hello my name is ${name}  and my position is web developer`)
+
+
+const gameName = new String("hitesh")
+
+console.log(name[0]);
+// console.log(name.length);
+// console.log(name.charAt(2));
+// console.log(name.indexOf('p'));
+
+const newString = name.slice(-1,1);
+console.log(newString)
