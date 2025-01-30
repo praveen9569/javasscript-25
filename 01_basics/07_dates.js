29b669a8d73e5ce74b5c818213460688a9e79a97
praveen9569 <144908644+praveen9569@users.noreply.github.com>
2025-01-30 09:29:43 +0000

    date and time datatypes

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,9 @@
+let myDate = new Date();
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+// timestamp // 
+
+// let mytime = Date.now()
+// console.log(mytime);
