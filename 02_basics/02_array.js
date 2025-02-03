0daa8db6e6e1d8b456084b1e893c7c91722bd1d0
praveen9569 <144908644+praveen9569@users.noreply.github.com>
2025-02-03 05:44:42 +0000

    array methos concat,from, of,

---
+++ 02_basics/02_array.js
@@ -0,0 +1,30 @@
+const marvel = ["iron man","thor","loki"];
+const dc_hero = ["superman","flash","batman"];
+
+
+// marvel.push(dc_hero);
+// console.log(marvel);
+
+//  const  newvar = marvel.concat(dc_hero);   // concat use another aray to store the new string;
+// console.log(newvar);
+
+// const all_hero = [...marvel,  ...dc_hero];
+// console.log(all_hero)
+
+// const another  = [1,2,3,[1.2,3,],[23,[1,4,2]]];
+// const newa = another.flat(5);
+
+// console.log(newa);
+
+// console.log(Array.from("hitesh"))
+// console.log(Array.isArray("hitesh"))
+// console.log(Array.from({name: "hitesh"})) // intersesting
+
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+console.log(Array.of(score1,score2,score3));
+
+
+
