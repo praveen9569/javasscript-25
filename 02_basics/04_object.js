9e966368ead920d4f9c367e296bbc84d78bfed66
praveen9569 <144908644+praveen9569@users.noreply.github.com>
2025-02-03 06:39:06 +0000

    object addintion and the therir assigment and  their  working

---
+++ 02_basics/04_object.js
@@ -0,0 +1,15 @@
+// const tinder  = new Object();  //singelton object
+
+// const tinder = {}   //non-singleton object
+
+// console.log(tinder);
+
+
+const obj1 = {1:"a",2:"b",3:"c"}
+const obj2 = {4:"d",5:"e",6:"f"}
+const obj3 = {7:"g",8:"h",9:"j"}
+
+const obj4 = {...obj1,...obj2,...obj3};
+console.log(obj4)
+console.log(Object.keys(obj4))
+console.log(Object.values(obj4))
