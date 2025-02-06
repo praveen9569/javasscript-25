d80ae3b295219566f1f6837782b978f3d157f315
praveen9569 <144908644+praveen9569@users.noreply.github.com>
2025-02-06 00:57:09 +0000

    objects detail

--- 02_basics/04_object.js
+++ 02_basics/04_object.js
@@ -9,7 +9,11 @@ const obj1 = {1:"a",2:"b",3:"c"}
 const obj2 = {4:"d",5:"e",6:"f"}
 const obj3 = {7:"g",8:"h",9:"j"}
 
-const obj4 = {...obj1,...obj2,...obj3};
+//const obj4 = {...obj1,...obj2,...obj3}; --> spread operator
+const obj4 =Object.assign({},obj1,obj2,obj3)
 console.log(obj4)
 console.log(Object.keys(obj4))
 console.log(Object.values(obj4))
+
+
+ 
