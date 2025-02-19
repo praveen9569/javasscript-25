b21aa18df954041105b79b48eb4862198680621f
praveen9569 <144908644+praveen9569@users.noreply.github.com>
2025-02-19 19:02:02 +0000

    passing object and array in function

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -10,10 +10,35 @@ function hello()
 // }
 // add(2,"a");
 
-function user(username)
+// function user(username)
+// {
+
+//     return `${username} just logged in `
+
+// }
+// console.log(user("praveen"));
+
+// function cart(...num1) ///here triple dot is called the rest operator and the spread operator
+// {
+//     return num1
+// }
+// console.log(cart(6,8,10))
+
+//  const user ={
+//     name: "rahul",
+//     age : 25
+// }
+
+// function objectpass(object)
+// {
+//     console.log(`${user.age}  and ${user.name}`)
+
+// }
+// objectpass(user);
+
+const array =[1,2,3,45];
+function arrayob(ab)
 {
-
-    return `${username} just logged in `
-
+    return array[0];
 }
-console.log(user("praveen"));
+console.log(arrayob(array));
