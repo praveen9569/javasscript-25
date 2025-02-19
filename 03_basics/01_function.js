6b58f7c8aafe551f1bc7ae955440e505f6d3731d
praveen9569 <144908644+praveen9569@users.noreply.github.com>
2025-02-19 18:45:55 +0000

    finction defintion and the function calling

---
+++ 03_basics/01_function.js
@@ -0,0 +1,19 @@
+function hello()
+{
+    console.log("hello");
+}
+
+//hello();
+// function add(number1,number2) // in function defenition we called the parameter and in function calling we called the aargument
+// {
+//     console.log(number1+number2);
+// }
+// add(2,"a");
+
+function user(username)
+{
+
+    return `${username} just logged in `
+
+}
+console.log(user("praveen"));
