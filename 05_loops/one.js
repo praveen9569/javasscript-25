78c011a134cefe1108305e2b63725145b8cdd8d0
praveen9569 <144908644+praveen9569@users.noreply.github.com>
2025-03-22 03:20:02 +0000

    loops

---
+++ 05_loops/one.js
@@ -0,0 +1,18 @@
+// for loop
+
+// for(let i=0;i<10;i++)
+// {
+//     console.log(i);
+//     console.log(" ");
+// }
+
+for(let i=1;i<10;i++)
+{
+
+    console.log(`table of ${i}`);
+    console.log( ' ');
+    for(let j=1;j<10;j++)
+    {
+    console.log( i +' * '+ j+ ' = '+i*j ); 
+    }
+}
