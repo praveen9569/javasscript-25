00aeb5341a79de5412f5eabf7c17aa70967192c4
praveen9569 <144908644+praveen9569@users.noreply.github.com>
2025-03-22 02:56:25 +0000

    control flow basics

---
+++ 04_control_flow/one.js
@@ -0,0 +1,29 @@
+// if(2=="2")
+// {
+//     console.log("running ");
+
+// }
+
+// if(2==="2")
+//     {
+//         console.log("running ");
+        
+//     }
+//     else{
+//         console.log("not running")
+//     }
+
+// const score =200;
+// if(score){
+//     const power = 'fly';
+//     console.log(`user power : ${power}`);
+// }
+
+
+const balance = 1000;
+// if(balance>500) console.log("test passed");
+
+if(balance>500)
+{
+    console.log(`balance is greater than the ${balance}`);
+}
---
+++ 04_control_flow/truth_false.js
@@ -0,0 +1,40 @@
+// const userEmail = "hitesh@gmail.com";
+
+// if(userEmail)
+// {
+//     console.log("gor the user email");
+// }
+// else{
+//     console.log("dont have user email");
+//}
+
+
+//falsey value  
+//   false , 0 , BigInt , "" , null ,undefined , NaN
+
+// //TRUTH VLAUES
+//   "0" , 'false' , ' ' , " " , [] , {} , function(){}
+
+
+// nullish coalescing operator (??):null undefined
+// let val1 ;
+// val1 = 5 ?? 10;
+
+
+// console.log(val1);
+
+// let val1 ;
+// val1 = null ?? 10;
+
+
+// console.log(val1);
+
+
+//   ....... terniary operator ....... //
+
+// condition ? true : false;
+
+
+const ice =100;
+
+(ice>100) ? console.log("run") :console.log(" not run");
