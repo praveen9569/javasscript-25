4d4bd0d3f94c39900e91b51836680516c0ff1b2e
praveen9569 <144908644+praveen9569@users.noreply.github.com>
2025-03-22 03:48:48 +0000

    loops more information

---
+++ 05_loops/three.js
@@ -0,0 +1,17 @@
+// const myObject = {
+//     js: 'javascript',
+//     cpp : 'c++',
+//     swift : 'swift'
+// }
+
+// for(const key in myObject)
+// {
+//     console.log(key);
+//     console.log(myObject[key])
+// }
+
+const arr = [1,2,3,4,5,6,7,8,9];
+for(const i in arr)
+{
+    console.log(arr[i]);
+}
